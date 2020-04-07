627fae83393a30321f17e90f268815d4f957746b
Eschechola <lucas.eschechola@outlook.com>
2020-04-06 23:11:55 -0300

    OOP - Parte 1

---
+++ FundamentosArquitetura/OOP/2 - Pilares OOP/EstadoComportamento.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    public class EstadoComportamento
+    {
+        //estado são suas propriedades
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+
+
+        //comportamento é o processamento
+        //pode alterar o estado da classe
+        public int CalcularIdade()
+        {
+            var dataAtual = DateTime.Now;
+
+            if (dataAtual > DataNascimento)
+                return dataAtual.Year - DataNascimento.Year;
+            
+            else
+                return 0;
+        }
+    }
+}
---
+++ FundamentosArquitetura/OOP/2 - Pilares OOP/Polimorfismo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    public class Polimorfismo
+    {}
+
+    //Poli - Morfismo
+    //Muitos comportamentos para algo
+
+    public class CafeteiraEspressa : Eletrodomestico
+    {
+        public CafeteiraEspressa(string nome, int voltagem)
+        : base(nome, voltagem)
+        {
+
+        }
+
+        public static void AquecerAgua() { }
+        public static void MoerGraos() { }
+
+        public void PrepararCafe()
+        {
+            Testar();
+            AquecerAgua();
+            MoerGraos();
+        }
+
+        //método com comportamento padrão
+        public override void Testar()
+        {
+            // testar cafeteira
+        }
+
+
+        //métodos abstratos sem implementação
+        public override void Desligar()
+        {
+            // resfriar aquecedor
+        }
+
+        public override void Ligar()
+        {
+            // verificar recipiente de agua
+        }
+
+    }
+}
